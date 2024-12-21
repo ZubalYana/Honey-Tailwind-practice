f5400617d25f7b34e1a686ade8d2b0bafb5757c7
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-12-21 10:36:19 +0200

    apply fonts, finish the header

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ export default function App() {
       <header className='w-full flex items-center justify-between px-12 py-8'>
         <img src={logo} alt="" />
         <ul className='w-[415px] flex justify-between items-center'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Our Product</li>
-          <li>Contact</li>
+          <li className='font-bold cursor-pointer'>Home</li>
+          <li className='font-light cursor-pointer'>About</li>
+          <li className='font-light cursor-pointer'>Our Product</li>
+          <li className='font-light cursor-pointer'>Contact</li>
         </ul>
       </header>
      </div>
