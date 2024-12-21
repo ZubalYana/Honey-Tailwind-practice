c3ec5933e146368eba70bf6a4320db47c77ddc45
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-12-21 10:30:32 +0200

    create the basic header

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import logo from '/logo.svg'
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <div className="wrap w-full">
+      <header className='w-full flex items-center justify-between px-12 py-8'>
+        <img src={logo} alt="" />
+        <ul className='w-[415px] flex justify-between items-center'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Our Product</li>
+          <li>Contact</li>
+        </ul>
+      </header>
+     </div>
     </>
   )
 }
-
-export default App
