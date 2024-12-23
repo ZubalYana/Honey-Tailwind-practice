15d8b859fa34f57f5bfa62428e919d591e21d067
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-12-23 06:02:05 +0200

    complete all the banner inner content

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,8 @@ export default function App() {
         <img src={honeySpoon} alt="honey spoon" className='w-[150px] absolute top-[10px] left-[-20px]' />
         <div className="mainPage_banner w-full h-full bg-gradient-to-tr from-honeyStart to-honeyEnd rounded-3xl flex items-center justify-center">
           <img src={logo} alt="honey" className='w-[65%] absolute z-10' />
-          <div className="banner_infoContent z-20 w-[60%] p-14 flex justify-center items-center">
-            <div className="titleContainer font-semibold text-7xl w-[70%] h-[280px] flex flex-col justify-between">
+          <div className="banner_infoContent z-20 w-[60%] px-40 flex justify-center flex-col">
+            <div className="titleContainer font-semibold text-7xl w-[70%] h-[280px] flex flex-col justify-between my-9">
               <div className="titleWord">Finger</div>
               <div className="titleWord mx-36">licking</div>
               <div className="titleWord flex items-end">
@@ -31,6 +31,10 @@ export default function App() {
                 </div>
                 <p className='mx-8'>honey!</p>
                 </div>
+            </div>
+            <div className="buttonsContainer w-[360px] flex justify-between">
+              <button className='w-[170px] h-[50px] bg-black text-white font-medium'>Buy Now</button>
+              <button className='w-[170px] h-[50px] text-black border-2 border-black font-medium'>Learn More</button>
             </div>
           </div>
           <div className="banner_imgContainer w-[40%] z-20">
