304a4ec84d3d103dbbaf8b8066180ab8f6287772
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-12-23 06:22:08 +0200

    import all the logos, complete our trusted shops section

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,17 @@ import Header from './components/Header/Header'
 import honeySpoon from '/honey spoon.png'
 import bannerHoney from '/banner honey.png'
 import bee from '/bee.png'
+import shopLogo1 from '/shopLogo1.svg'
+import shopLogo2 from '/shopLogo2.svg'
+import shopLogo3 from '/shopLogo3.svg'
+import shopLogo4 from '/shopLogo4.svg'
 export default function App() {
   return (
     <>
      <div className="wrap w-full">
      <Header />
+
+     {/* main page start */}
       <section className='mainPage w-full h-[78vh] px-12 relative'>
         <img src={honeySpoon} alt="honey spoon" className='w-[150px] absolute top-[10px] left-[-20px]' />
         <div className="mainPage_banner w-full h-full bg-gradient-to-tr from-honeyStart to-honeyEnd rounded-3xl flex items-center justify-center">
@@ -35,6 +41,20 @@ export default function App() {
           </div>
         </div>
       </section>
+     {/* main page end */}
+
+     {/* trusted shops page start */}
+      <section className='trustedShopsPage w-full h-[45vh] py-12 px-56 relative'>
+        <h3 className='text-3xl font-semibold text-black my-9'>Our Trusted Shops</h3>
+        <div className="shopLogosCon flex justify-between items-center">
+          <img src={shopLogo1} alt="shop logo 1" />
+          <img src={shopLogo2} alt="shop logo 2" />
+          <img src={shopLogo3} alt="shop logo 3" />
+          <img src={shopLogo4} alt="shop logo 4" />
+        </div>
+      </section>
+     {/* trusted shops page end */}
+
      </div>
     </>
   )
